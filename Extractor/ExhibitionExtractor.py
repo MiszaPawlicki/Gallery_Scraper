9ad2ad0af0f735da367000d58d864358baa02412
MiszaPawlicki <miszapawlicki7@gmail.com>
2024-05-06 19:11:08 +0100

    Playing around - added more year related features - most features must now be within the path - training size tweaked to 0.3 - base_url id feature added - checking whether year in path as well as at end of path to resolve lisson gallery issues

--- Extractor/ExhibitionExtractor.py
+++ Extractor/ExhibitionExtractor.py
@@ -2,17 +2,17 @@ import WebScraping.GalleryScraper as scraper
 import ML.LogisticalRegression as lr
 def get_exhibition_urls(url):
 
-    model, vectorizer = lr.load_model()
     site_hrefs = scraper.scrape_hrefs(url)
     exhibitions = []
 
     for href in site_hrefs:
-        is_exhibition = lr.predict_url(model, vectorizer, href)
+        is_exhibition = lr.predict_url(href)
         if is_exhibition:
             exhibitions.append(href)
-            print(href)
 
-    return exhibitions
+
+    unique_exhibitions = list(set(exhibitions))
+    return unique_exhibitions
 
 def scrape_exhibiton_details():
     #This function extract all info from the individual exhibition page
@@ -21,26 +21,25 @@ def scrape_exhibiton_details():
 
 def test():
     exhibitions = [
-        'https://www.southbankcentre.co.uk/whats-on',
+        'https://www.southbankcentre.co.uk/whats-on?type=art-exhibitions',
         'https://www.lissongallery.com/exhibitions',
-        'https://www.tate.org.uk/whats-on',
+        'https://www.tate.org.uk/whats-on?event_type=exhibition',
         'https://www.barbican.org.uk/whats-on/art-design',
         'https://www.saatchigallery.com/whats-on',
-        'https://www.somersethouse.org.uk/whats-on',
-        'https://www.npg.org.uk/whatson/events-calendar',
-        'https://www.dulwichpicturegallery.org.uk/whats-on/exhibitions/',
-        'https://camdenartcentre.org/whats-on/in-the-building/exhibitions'
+        #'https://www.somersethouse.org.uk/whats-on',
+        #'https://www.npg.org.uk/whatson/events-calendar',
+        #'https://www.dulwichpicturegallery.org.uk/whats-on/exhibitions/',
+        #'https://camdenartcentre.org/whats-on/in-the-building/exhibitions'
 
     ]
 
-
-    all_exhibitions = []
     for url in exhibitions:
+        print('url: ' + url)
         found_exhibitions = get_exhibition_urls(url)
         if found_exhibitions:
             for e in found_exhibitions:
-                all_exhibitions.append(e)
+                print(e)
 
-    for e in  all_exhibitions:
-        print(e)
 
+
+test()
--- ML/LogisticalRegression.py
+++ ML/LogisticalRegression.py
@@ -4,14 +4,12 @@ from sklearn.linear_model import LogisticRegression
 import joblib
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
-from ML.Featurizers import UrlFeaturizer
+from ML.UrlFeaturizer import UrlFeaturizer
 
-#TODO refactor so model is only loaded once
-#TODO get rid of warning on prediction
-#TODO add more features to improve performance
+#TODO refactor so loads only occur once
+#TODO adjust features to improve performance
 
 def train_logistical_regression_model(csv_path):
-
     # Load the dataset
     df = pd.read_csv(csv_path)
 
@@ -19,11 +17,12 @@ def train_logistical_regression_model(csv_path):
     X = []
 
     # Initialize the UrlFeaturizer
-    featurizer = UrlFeaturizer("")
+
 
     # Extract features using the UrlFeaturizer for each URL in the dataset
     for url in df['href']:
-        featurizer.url = url
+        featurizer = UrlFeaturizer(url)
+        featurizer.path = url
         features = featurizer.run()
         X.append(features)
 
@@ -34,7 +33,7 @@ def train_logistical_regression_model(csv_path):
     y = df['is_exhibition']
 
     # Split the dataset into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
     # Initialize and train a Logistic Regression model
     lr_model = LogisticRegression()
@@ -49,29 +48,38 @@ def train_logistical_regression_model(csv_path):
     print("Classification Report:")
     print(classification_report(y_test, y_pred))
 
-    # Save the model and featurizer
+    # Save the model and featurizer along with feature names
     model_dir = 'models'
     os.makedirs(model_dir, exist_ok=True)
     model_path = os.path.join(model_dir, 'logistic_regression_model.pkl')
     featurizer_path = os.path.join(model_dir, 'featurizer.pkl')
     joblib.dump(lr_model, model_path)
-    joblib.dump(featurizer, featurizer_path)
+    with open(featurizer_path, 'wb') as f:
+        joblib.dump((featurizer, X.columns.tolist()), f)  # Store feature names from DataFrame columns
     print("Model and featurizer saved!")
 
 
+def predict_url(url):
+    # Load the model and featurizer with feature names
+    model_path = r'C:\Users\misza\OneDrive\Documents\Work\Personal Projects\Gallery Scraper\ML\models\logistic_regression_model.pkl'
+    featurizer_path = r'C:\Users\misza\OneDrive\Documents\Work\Personal Projects\Gallery Scraper\ML\models\featurizer.pkl'
+    with open(featurizer_path, 'rb') as f:
+        featurizer, feature_names = joblib.load(f)
 
-def predict_url(model, featurizer, url):
     # Initialize the UrlFeaturizer with the new URL
-    featurizer.url = url
+    featurizer.path = url
 
     # Extract features for the new URL
     features = featurizer.run()
 
-    # Convert the features dictionary into a 2D array
-    X = []
-    for value in features.values():
-        X.append(value)
-    X = [X]  # Convert to a list of lists
+    # Convert features to a DataFrame
+    features_df = pd.DataFrame([features])
+
+    # Select features based on their names
+    X = features_df[feature_names]
+
+    # Load the model
+    model = joblib.load(model_path)
 
     # Make prediction using the trained model
     prediction = model.predict(X)
@@ -80,31 +88,27 @@ def predict_url(model, featurizer, url):
     return prediction[0]
 
 
-def load_model():
-    model = joblib.load(r'C:\Users\misza\OneDrive\Documents\Work\Personal Projects\Gallery Scraper\ML\models\logistic_regression_model.pkl')
-    featurizer = UrlFeaturizer("")
-    print("Model and featurizer loaded successfully!")
-    return model, featurizer
-
-
 # Example usage
 def main():
     # Paths
     csv_path = r'C:\Users\misza\OneDrive\Documents\Work\Personal Projects\Gallery Scraper\ML\training_data\exhibition_href_training.csv'
 
+    # Train the model
     train_logistical_regression_model(csv_path)
 
-    model, featurizer = load_model()
-
     url_to_predict = 'https://www.barbican.org.uk/whats-on/2024/event/this-exhibition'
-    print(predict_url(model, featurizer, url_to_predict))
+    print(predict_url(url_to_predict))
 
     url_to_predict = 'https://www.barbican.org.uk/whats-on/past/event/this-exhibition'
-    print(predict_url(model, featurizer, url_to_predict))
+    print(predict_url(url_to_predict))
 
     url_to_predict = 'https://www.barbican.org.uk/about'
-    print(predict_url(model, featurizer, url_to_predict))
+    print(predict_url(url_to_predict))
+
+    url_to_predict = 'https://www.lissongallery.com/exhibitions/otobong-nkanga'
+    print(predict_url(url_to_predict))
 
 
 if __name__ == "__main__":
     main()
+
