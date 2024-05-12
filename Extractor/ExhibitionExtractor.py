dd74819d79833d2282177ce1f20351d46e1353ac
MiszaPawlicki <miszapawlicki7@gmail.com>
2024-05-12 21:27:24 +0100

    Introducing Random Forrest - Having specific criteria per domain made logistical regression a bad approach for this problem. A random forrest model should perform better in theory as it will better recognise the importance of base domains and what must follow them to identify an exhibition. Mostly thanks to the decision tree nature of it

--- Extractor/ExhibitionExtractor.py
+++ Extractor/ExhibitionExtractor.py
@@ -1,12 +1,12 @@
 import WebScraping.GalleryScraper as scraper
-import ML.LogisticalRegression as lr
+import ML.RandomForrest as rf
 def get_exhibition_urls(url):
 
     site_hrefs = scraper.scrape_hrefs(url)
     exhibitions = []
 
     for href in site_hrefs:
-        is_exhibition = lr.predict_url(href)
+        is_exhibition = rf.predict_url(href)
         if is_exhibition:
             exhibitions.append(href)
 
@@ -26,10 +26,10 @@ def test():
         'https://www.tate.org.uk/whats-on?event_type=exhibition',
         'https://www.barbican.org.uk/whats-on/art-design',
         'https://www.saatchigallery.com/whats-on',
-        #'https://www.somersethouse.org.uk/whats-on',
-        #'https://www.npg.org.uk/whatson/events-calendar',
-        #'https://www.dulwichpicturegallery.org.uk/whats-on/exhibitions/',
-        #'https://camdenartcentre.org/whats-on/in-the-building/exhibitions'
+        'https://www.somersethouse.org.uk/whats-on',
+        'https://www.npg.org.uk/whatson/events-calendar',
+        'https://www.dulwichpicturegallery.org.uk/whats-on/exhibitions/',
+        'https://camdenartcentre.org/whats-on/in-the-building/exhibitions'
 
     ]
 
--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-from WebScraping.GalleryScraper import Scraper
+import csv
 
-scraper = Scraper('https://bbc.co.uk')
-print(scraper.scrape('https://bbc.co.uk'))
+def read_unique_values(csv_file):
+    unique_values = set()  # Using a set to store unique rows
+
+    with open(csv_file, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip the header row
+        for row in reader:
+            unique_values.add(tuple(row))  # Convert row to tuple to ensure uniqueness
+
+    return unique_values
+
+# Example usage:
+csv_file = r"C:\Users\misza\OneDrive\Documents\Work\Personal Projects\Gallery Scraper\ML\training_data\exhibition_href_training.csv"  # Replace with the path to your CSV file
+unique_rows = read_unique_values(csv_file)
+for row in unique_rows:
+    print(f"{row[0]}, {row[1]}")
